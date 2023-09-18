7c7885babbb05d894a72ef3fbe1860acedf88e9b
AnsarKhubiev <anskhubiev@icloud.com>
2023-09-18 22:01:44 +0300

    test

--- main.js
+++ main.js
@@ -592,21 +592,44 @@ function fun1() {
 
 // showMinNumber(6, 6);
 
-function getNumbers() {
-    let numb = 0;
-    while (numb < 1 || numb % 1 !== 0) {
-        alert("Введите целое число больше 0");
-        numb = parseInt(prompt("Введите число"));
+// function getNumbers() {
+//     let numb = 0;
+//     while (numb < 1 || numb % 1 !== 0) {
+//         alert("Введите целое число больше 0");
+//         numb = parseInt(prompt("Введите число"));
+//     }
+//     return numb;
+// }
+
+// function pow() {
+//     const numb1 = getNumbers();
+//     const numb2 = getNumbers();
+
+//     result = numb1 ** numb2;
+//     alert(result);
+// }
+
+// pow();
+
+function ask(question, yes, no) {
+    if (confirm(question())) {
+        yes();
+    } else {
+        no();
     }
-    return numb;
 }
 
-function pow() {
-    const numb1 = getNumbers();
-    const numb2 = getNumbers();
-
-    result = numb1 ** numb2;
-    alert(result);
+function question() {
+    return 'Вы хотите спать?';
 }
 
-pow();
+function sayYeas() {
+    alert("Ложитесь спать");
+}
+
+function sayNo() {
+    alert("Поработайте");
+}
+
+
+ask(question, sayYeas, sayNo)
